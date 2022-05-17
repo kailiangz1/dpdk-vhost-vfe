dd35d24bc6757d4d78f17acefdd0119d732dba6a
Li Zhang <lizh@nvidia.com>
2022-05-17 11:06:10 +0300

    jsonrpc: support RPC operations function callback
    Driver provides function callback to operation struction.
    So that APP can call it just as the rte_vdpa_dev_ops structure did.
    
    Add structure rte_rpc_vdpa_global_ops to
    driver register callback functions.
    Add following API to add PF/VF device:
    rte_rpc_vdpa_global_ops_init
    rte_vdpa_pf_dev_add
    rte_vdpa_pf_dev_remove
    rte_vdpa_get_pf_list
    rte_vdpa_pf_dev_vf_dev_prov
    rte_vdpa_pf_dev_vf_dev_add
    rte_vdpa_pf_dev_vf_dev_remove
    rte_vdpa_get_vf_list
    rte_vdpa_get_vf_info
    
    Signed-off-by: Li Zhang <lizh@nvidia.com>

---
+++ lib/jsonrpc/rte_rpc.c
@@ -0,0 +1,94 @@
+/* SPDX-License-Identifier: BSD-3-Clause
+ * Copyright 2021, NVIDIA CORPORATION & AFFILIATES.
+ */
+
+/**
+ * @file
+ *
+ * Device specific rpc lib
+ */
+#include "rte_rpc.h"
+
+static struct rte_rpc_vdpa_global_ops rte_rpc_vdpa_ops;
+
+void
+rte_rpc_vdpa_global_ops_init(struct rte_rpc_vdpa_global_ops *ops)
+{
+	rte_rpc_vdpa_ops = (*ops);
+}
+
+/* Macros to check for invalid function pointers */
+#define RTE_GLOBAL_FUNC_PTR_ERR_RET(func, retval) do { \
+	if (rte_rpc_vdpa_ops.func == NULL) \
+		return retval; \
+} while (0)
+
+int
+rte_vdpa_pf_dev_add(const char *pf_name)
+{
+	RTE_GLOBAL_FUNC_PTR_ERR_RET(pf_dev_add, -ENOTSUP);
+
+	return rte_rpc_vdpa_ops.pf_dev_add(pf_name);
+}
+
+int
+rte_vdpa_pf_dev_remove(const char *pf_name)
+{
+	RTE_GLOBAL_FUNC_PTR_ERR_RET(pf_dev_remove, -ENOTSUP);
+
+	return rte_rpc_vdpa_ops.pf_dev_remove(pf_name);
+}
+
+int
+rte_vdpa_get_pf_list(struct vdpa_pf_info *pf_info, int max_pf_num)
+{
+	RTE_GLOBAL_FUNC_PTR_ERR_RET(get_pf_list, -ENOTSUP);
+
+	return rte_rpc_vdpa_ops.get_pf_list(pf_info, max_pf_num);
+}
+
+int
+rte_vdpa_pf_dev_vf_dev_prov(const char *pf_name, uint32_t vfid,
+			struct vdpa_vf_params *vf_params)
+{
+	RTE_GLOBAL_FUNC_PTR_ERR_RET(pf_dev_vf_dev_prov, -ENOTSUP);
+
+	return rte_rpc_vdpa_ops.pf_dev_vf_dev_prov(pf_name,
+				vfid, vf_params);
+}
+
+int
+rte_vdpa_pf_dev_vf_dev_add(const char *pf_name, uint32_t vfid,
+			struct vdpa_vf_params *vf_params)
+{
+	RTE_GLOBAL_FUNC_PTR_ERR_RET(pf_dev_vf_dev_add, -ENOTSUP);
+
+	return rte_rpc_vdpa_ops.pf_dev_vf_dev_add(pf_name,
+				vfid, vf_params);
+}
+
+int
+rte_vdpa_pf_dev_vf_dev_remove(const char *pf_name, uint32_t vfid)
+{
+	RTE_GLOBAL_FUNC_PTR_ERR_RET(pf_dev_vf_dev_remove, -ENOTSUP);
+
+	return rte_rpc_vdpa_ops.pf_dev_vf_dev_remove(pf_name, vfid);
+}
+
+int
+rte_vdpa_get_vf_list(const char *pf_name, struct vdpa_vf_info *vf_info,
+		int max_vf_num)
+{
+	RTE_GLOBAL_FUNC_PTR_ERR_RET(get_vf_list, -ENOTSUP);
+
+	return rte_rpc_vdpa_ops.get_vf_list(pf_name, vf_info, max_vf_num);
+}
+
+int
+rte_vdpa_get_vf_info(const char *pf_name, uint32_t vfid,
+		struct vdpa_vf_info *vf_info)
+{
+	RTE_GLOBAL_FUNC_PTR_ERR_RET(get_vf_info, -ENOTSUP);
+
+	return rte_rpc_vdpa_ops.get_vf_info(pf_name, vfid, vf_info);
+}
---
+++ lib/jsonrpc/rte_rpc.h
@@ -0,0 +1,118 @@
+/* SPDX-License-Identifier: BSD-3-Clause
+ * Copyright 2021, NVIDIA CORPORATION & AFFILIATES.
+ */
+
+#ifndef _RTE_RPC_H_
+#define _RTE_RPC_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * @file
+ *
+ * Device specific rpc lib
+ */
+
+#include <stdint.h>
+#include <rte_compat.h>
+#include <rte_dev.h>
+#include <rte_ether.h>
+
+struct vdpa_pf_info {
+	char pf_name[RTE_DEV_NAME_MAX_LEN];
+};
+
+struct vdpa_vf_params {
+	char vf_name[RTE_DEV_NAME_MAX_LEN];
+	uint32_t prov_flags;
+	uint32_t msix_num;
+	uint32_t queue_num;
+	uint32_t queue_size;
+	uint64_t features;
+	uint32_t mtu;
+	struct rte_ether_addr mac;
+};
+
+struct vdpa_vf_info {
+	uint32_t vfid;
+	struct vdpa_vf_params vf_params;
+};
+
+enum vdpa_vf_prov_flags {
+	VDPA_VF_MSIX_NUM,
+	VDPA_VF_QUEUE_NUM,
+	VDPA_VF_QUEUE_SIZE,
+	VDPA_VF_FEATURES,
+	VDPA_VF_MTU,
+	VDPA_VF_MAC,
+};
+
+struct rte_rpc_vdpa_global_ops {
+	/** Add pf to system */
+	int (*pf_dev_add)(const char *pf_name);
+
+	/** Remove pf from system */
+	int (*pf_dev_remove)(const char *pf_name);
+
+	/** Get info of pfs */
+	int (*get_pf_list)(struct vdpa_pf_info *pf_info, int max_pf_num);
+
+	/** Add vf provision to system and associate with pf */
+	int (*pf_dev_vf_dev_prov)(const char *pf_name, uint32_t vfid,
+			struct vdpa_vf_params *vf_params);
+
+	/** Add vf to system and associate with pf, vf_params maybe NULL */
+	int (*pf_dev_vf_dev_add)(const char *pf_name, uint32_t vfid,
+			struct vdpa_vf_params *vf_params);
+
+	/** Remove vf from system */
+	int (*pf_dev_vf_dev_remove)(const char *pf_name, uint32_t vfid);
+
+	/** Get info of vfs under pf */
+	int (*get_vf_list)(const char *pf_name,
+		struct vdpa_vf_info *vf_info, int max_vf_num);
+
+	/** Get info of vfs */
+	int (*get_vf_info)(const char *pf_name, uint32_t vfid,
+		struct vdpa_vf_info *vf_info);
+};
+
+__rte_internal
+void
+rte_rpc_vdpa_global_ops_init(struct rte_rpc_vdpa_global_ops *ops);
+
+int
+rte_vdpa_pf_dev_add(const char *pf_name);
+
+int
+rte_vdpa_pf_dev_remove(const char *pf_name);
+
+int
+rte_vdpa_get_pf_list(struct vdpa_pf_info *pf_info, int max_pf_num);
+
+int
+rte_vdpa_pf_dev_vf_dev_prov(const char *pf_name, uint32_t vfid,
+			struct vdpa_vf_params *vf_params);
+
+int
+rte_vdpa_pf_dev_vf_dev_add(const char *pf_name, uint32_t vfid,
+			struct vdpa_vf_params *vf_params);
+
+int
+rte_vdpa_pf_dev_vf_dev_remove(const char *pf_name, uint32_t vfid);
+
+int
+rte_vdpa_get_vf_list(const char *pf_name, struct vdpa_vf_info *vf_info,
+		int max_vf_num);
+
+int
+rte_vdpa_get_vf_info(const char *pf_name, uint32_t vfid,
+		struct vdpa_vf_info *vf_info);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _RTE_RPC_H_ */
